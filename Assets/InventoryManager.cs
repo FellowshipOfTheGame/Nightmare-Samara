5ab31ace556b8e23ae8f619e92acfbf63c40432b
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2024-11-22 17:37:01 -0300

    Implementado a função que randomiza o item que vai ser pego na caixa

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -6,28 +6,44 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    //bool hasWeapon = false;
-    //bool hasRange = false;
+    private int poisonFlask;
+    private int woodenBat;
+    //private bool hasLifeFlask;
 
-
-    private void Update()
+    private void Start()
     {
-        /*
-        if (hasWeapon)
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift) && hasRange)
-            {
-                Debug.Log("BATEU");
-            }
-        }
-        */
+        poisonFlask = 0;    
+        woodenBat = 0;
+        //hasLifeFlask = false;
     }
 
+
+    private void ItemRandomizer()
+    {
+        int randomValue = Random.Range(0, 100);
+        if (randomValue < 80)
+        {
+            // PoisonFlask
+            poisonFlask++;
+            Debug.Log($"O número de venenos agora é: {poisonFlask}");
+        }
+        else if (randomValue < 90) // 80 a 89 (10% de chance)
+        {
+            // WoodenBat
+            woodenBat++;
+            Debug.Log($"O número de tacos agora é: {woodenBat}");
+        }
+        else // 90 a 99 (10% de chance)
+        {
+            // Nothing
+            Debug.Log("Nada");
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Item"))
+        if (collision.gameObject.CompareTag("Box"))
         {
-            //hasWeapon = true;
+            ItemRandomizer();
             Destroy(collision.gameObject);
         }
     }
