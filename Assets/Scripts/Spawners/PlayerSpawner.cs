95e8114e99edd9f4d41ab71cd7dc47b93b382b6f
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2025-07-14 15:43:48 -0300

    Adicionei o spawn aleatorio do jogador

---
+++ Assets/Scripts/Spawners/PlayerSpawner.cs
@@ -0,0 +1,37 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private Color gizmoColor = Color.green;
+    [SerializeField] private float gizmoRadius = 0.3f;
+
+    private List<Transform> spawnPoints = new List<Transform>();
+
+    void Start()
+    {
+        spawnPoints.Clear();
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            if (t != transform)
+                spawnPoints.Add(t);
+        }
+
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        GameObject playerInstance = Instantiate(playerPrefab, spawnPoints[randomIndex].position, Quaternion.identity);
+        virtualCamera.Follow = playerInstance.transform;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        foreach (Transform t in transform)
+        {
+            Gizmos.DrawSphere(t.position, gizmoRadius);
+        }
+    }
+}
