09a71f9d7fc5977e1653af00033a727dd2f376c6
Miguel Saffioti <miguel.zsaffioti@gmail.com>
2025-09-17 01:34:40 -0300

    Setup para iluminação

--- Assets/Scripts/NoiseEffect.cs
+++ Assets/Scripts/NoiseEffect.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-[ExecuteInEditMode]
-public class NoiseEffect : MonoBehaviour
+public class NoiseRenderFeature : ScriptableRendererFeature
 {
-    public Material noiseMaterial;
-
-    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    class NoisePass : ScriptableRenderPass
     {
-        if (noiseMaterial != null)
-            Graphics.Blit(src, dest, noiseMaterial);
-        else
-            Graphics.Blit(src, dest);
+        public Material material;
+        private RenderTargetHandle temporaryColorTexture;
+
+        public NoisePass(Material mat)
+        {
+            material = mat;
+            temporaryColorTexture.Init("_TempColorTexture");
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (material == null) return;
+
+            CommandBuffer cmd = CommandBufferPool.Get("NoiseEffect");
+
+            // ✅ Pegar a câmera target dentro do Execute, sem usar a variável da classe
+            var cameraTarget = renderingData.cameraData.renderer.cameraColorTarget;
+
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            cmd.GetTemporaryRT(temporaryColorTexture.id, opaqueDesc);
+
+            Blit(cmd, cameraTarget, temporaryColorTexture.Identifier(), material);
+            Blit(cmd, temporaryColorTexture.Identifier(), cameraTarget);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+
+    public Material noiseMaterial;
+    private NoisePass noisePass;
+
+    public override void Create()
+    {
+        noisePass = new NoisePass(noiseMaterial);
+        noisePass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        // ✅ Não acessa cameraColorTarget aqui, só enfileira o pass
+        renderer.EnqueuePass(noisePass);
     }
 }
-
